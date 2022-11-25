29a322f92443a525b9b6376be53ef77b61f1ac59
SornDev <sorndev.teach@gmail.com>
2022-11-25 18:37:57 +0700

    upload

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Laravel + Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyappApp',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
